807cf055ca13714d0eb4a52633f47a2017516c50
Bilal Hussain <bilal.hussain@incentro.com>
2017-07-12 22:33:51 +0200

    Initial commit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'app works!';
+  private apiURL = 'https://api.whatdoestrumpthink.com/api/v1/quotes/';
+  quote: string;
+  errorMessage: string;
+  min: number;
+  max: number;
+
+  constructor(private http: Http) {
+    this.getQuote();
+  }
+
+  getQuote() {
+    this.http.get(this.apiURL)
+    .map(res => res.json())
+    .subscribe(
+      data => {
+        this.quote = data.messages.non_personalized[this.getRandomNumber()];
+      },
+      error => {
+        if (error.status !== 200) {
+          this.errorMessage = 'IT IS BROKEN';
+        }
+      }
+    );
+    return false;
+  }
+
+  getRandomNumber() {
+    this.min = 0;
+    this.max = 47;
+    return Math.floor(Math.random() * (this.max - this.min + 1)) + this.min;
+  }
 }
